d73af5c242f4ad9de328c4d7ae099100d69548e0
Vidushi <61788101+Ackermann99@users.noreply.github.com>
2020-03-04 21:05:54 +0530

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+for i in range(2):
+    for j in range(i,2):
+        print(" ", end="")
+    for k in range(i+1):
+        print("*", end="")
+    print()
+print("* *")
+for i in range(3):
+    print("  *")
+print("*****")
+"""
+"""
+print("****")
+for i in range(2):
+    print("   *")
+print("****")
+for i in range(2):
+    print("*")
+print("****")
+"""
+"""
+print("****")
+for i in range(2):
+    print("   *")
+print("****")
+for i in range(2):
+    print("   *")
+print("****")
+"""
+for i in range(3):
+    print("*   *")
+print("*****")
+for i in range(3):
+    print("    *")
